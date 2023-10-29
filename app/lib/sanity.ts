0a057c1b5efe1e91c993826a219532eb4dadf780
Har1s-Akbar <stanker801@gmail.com>
2023-10-29 04:14:41 -0700

    fixing errors

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,9 +1,9 @@
-import { createClient } from "next-sanity";
+import { createClient } from "@sanity/client"
 
-const projectId = "2hdzu35m";
-const dataset = "production";
-const apiVersion = "2023-01-01";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const token = process.env.NEXT_PUBLIC_SANITY_EDITOR_TOKEN
+const apiVersion = "2023-01-01";
 
 export const client = createClient({
   projectId,
